9fac367fd05c08a3569ae4e3d411510d7f6b469d
kylung0208 <kylung2008@gmail.com>
2020-06-15 00:18:09 +0800

    add intro to jQuert in

---
+++ jQuery-intro/selector_exercise.js
@@ -0,0 +1,11 @@
+// if(jQuery){
+//     alert('jQuery Loaded!')
+// }
+// else{
+//     alert('something wrong')
+// }
+
+$('div').css('background', 'purple')
+$('div.highlight').css('width', '200px')
+$('div#third').css('border', '2px solid orange')
+$('div:first-of-type').css("color", "pink")
